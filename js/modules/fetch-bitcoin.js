e20f5c5c35ea1f22b4adf7042798d9ffee3fb11c
Evelym Rondon <evionline@gmail.com>
2020-12-16 19:07:49 -0400

    mudança usuario2 adiciona console.log

--- js/modules/fetch-bitcoin.js
+++ js/modules/fetch-bitcoin.js
@@ -1,6 +1,10 @@
 export default function initFetchBitcoin() {
-  fetch('https://blockchain.info/ticker').then((response) => response.json()).then((bitcoin) => {
-    const btcPrice = document.querySelector('.btc-preco');
-    btcPrice.innerText = (1000 / bitcoin.BRL.sell).toFixed(4);
-  }).catch((error) => console.log(Error(error)));
+  console.log("teste");
+  fetch("https://blockchain.info/ticker")
+    .then((response) => response.json())
+    .then((bitcoin) => {
+      const btcPrice = document.querySelector(".btc-preco");
+      btcPrice.innerText = (1000 / bitcoin.BRL.sell).toFixed(4);
+    })
+    .catch((error) => console.log(Error(error)));
 }
