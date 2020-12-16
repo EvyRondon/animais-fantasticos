18c21d03108e39f286053de2e8ecd7962280158c
Evelym Rondon <evionline@gmail.com>
2020-12-16 15:42:57 -0400

    tabnav transformado em classe

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -12,14 +12,12 @@ export default class Accordion {
   // adiciona os eventos ao accordion
   addAccordionEvent() {
     this.accordionList.forEach((item) => {
-      console.log("item", item);
       item.addEventListener("click", () => this.toggleAccordion(item));
     });
   }
 
   // iniciar função
   init() {
-    console.log("teste");
     if (this.accordionList.length) {
       // ativar primeiro item
       this.toggleAccordion(this.accordionList[0]);
--- js/modules/scroll-suave.js
+++ js/modules/scroll-suave.js
@@ -14,7 +14,6 @@ export default class ScrollSuave {
   }
 
   scrollToSection(event) {
-    console.log(this.options);
     event.preventDefault();
     const href = event.currentTarget.getAttribute("href");
     const section = document.querySelector(href);
