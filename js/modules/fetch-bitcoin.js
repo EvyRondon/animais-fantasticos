406e8b5df30adc74573c20f4f690511b8152aa4e
Evelym Rondon <evionline@gmail.com>
2020-12-16 18:59:10 -0400

    Fetch bitcoin refatorado

--- js/modules/fetch-bitcoin.js
+++ js/modules/fetch-bitcoin.js
@@ -1,6 +1,9 @@
-export default function initFetchBitcoin() {
-  fetch('https://blockchain.info/ticker').then((response) => response.json()).then((bitcoin) => {
-    const btcPrice = document.querySelector('.btc-preco');
-    btcPrice.innerText = (1000 / bitcoin.BRL.sell).toFixed(4);
-  }).catch((error) => console.log(Error(error)));
+export default function fetchBitcoin(url, target) {
+  fetch(url)
+    .then((response) => response.json())
+    .then((bitcoin) => {
+      const btcPrice = document.querySelector(target);
+      btcPrice.innerText = (1000 / bitcoin.BRL.sell).toFixed(4);
+    })
+    .catch((error) => console.log(Error(error)));
 }
