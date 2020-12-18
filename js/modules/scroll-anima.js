b1987dd7f26a08c2b950dd108e14391c5ecb0761
Evelym Rondon <evionline@gmail.com>
2020-12-18 15:27:31 -0400

    adicionado debouce na animacao ao scroll

--- js/modules/scroll-anima.js
+++ js/modules/scroll-anima.js
@@ -1,9 +1,11 @@
+import debouce from "./debouce.js";
+
 export default class ScrollAnimation {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections);
     this.windowMetade = window.innerHeight * 0.6;
 
-    this.checkDistance = this.checkDistance.bind(this);
+    this.checkDistance = debouce(this.checkDistance.bind(this), 50);
   }
 
   getDistance() {
